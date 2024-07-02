a59fcbc310e67bdb5f69c73d3ee757a815cc6e3f
Gerald Versluis <gerald.versluis@microsoft.com>
2024-07-02 11:33:26 +0200

    Implement CameraView

--- MauiCameraViewSample/MainPage.xaml.cs
+++ MauiCameraViewSample/MainPage.xaml.cs
@@ -1,24 +1,40 @@
-﻿namespace MauiCameraViewSample
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Primitives;
+
+namespace MauiCameraViewSample
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void MyCamera_MediaCaptured(object sender, CommunityToolkit.Maui.Views.MediaCapturedEventArgs e)
         {
-            count++;
+            if (Dispatcher.IsDispatchRequired)
+            {
+                Dispatcher.Dispatch(() => MyImage.Source = ImageSource.FromStream(() => e.Media));
+                return;
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            MyImage.Source = ImageSource.FromStream(() => e.Media);
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await MyCamera.CaptureImage(CancellationToken.None);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            MyCamera.CameraFlashMode = MyCamera.CameraFlashMode == CameraFlashMode.Off ? 
+                CameraFlashMode.On : CameraFlashMode.Off;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            MyCamera.ZoomFactor += 0.1f;
         }
     }
 
--- MauiCameraViewSample/MauiProgram.cs
+++ MauiCameraViewSample/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MauiCameraViewSample
 {
@@ -9,6 +10,7 @@ namespace MauiCameraViewSample
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitCamera()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
